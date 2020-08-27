2b0e8bb2b44c8ffae62f4236d5bba12e8698df0d
Abdulkareem Mustapha <aphatheology@gmail.com>
2020-08-27 09:35:06 +0100

    Updates

---
+++ precache-manifest.bebdc340569be23ef33dfb0305e04e8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f6d7d6306ad16dabf67b71d95966b2b",
+    "url": "/RegistrationFormWithReact/index.html"
+  },
+  {
+    "revision": "2240ccd706090e8d23c1",
+    "url": "/RegistrationFormWithReact/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e6395fa474a26d2109e8",
+    "url": "/RegistrationFormWithReact/static/css/main.c7c63369.chunk.css"
+  },
+  {
+    "revision": "2240ccd706090e8d23c1",
+    "url": "/RegistrationFormWithReact/static/js/2.e38dbe55.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RegistrationFormWithReact/static/js/2.e38dbe55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6395fa474a26d2109e8",
+    "url": "/RegistrationFormWithReact/static/js/main.fdec6d3f.chunk.js"
+  },
+  {
+    "revision": "02866b522bffdd0f05ed",
+    "url": "/RegistrationFormWithReact/static/js/runtime-main.3940c504.js"
+  }
+]);
